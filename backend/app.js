41890e6b3a597296862794450bab8d2ccbadc985
Himanshu Bajpai <himanshubajpai0644@gmail.com>
2024-03-13 12:33:38 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express =require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors'); 
+import path from 'path';
 
 app.use(cors());
 
@@ -10,7 +11,7 @@ const errorMiddleware = require('./middleware/error');
 
 require('dotenv').config({path:'backend/config/config.env'});
 
-
+const __dirname = path.resolve()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
@@ -24,6 +25,10 @@ app.use('/api/v1',product);
 app.use('/api/v1',user);
 app.use('/api/v1',order);
 
+app.use(express.static(path.join(__dirname , '/frontend/build')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+})
 
 // middleware for error 
 app.use(errorMiddleware);
